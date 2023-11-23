15e96ea9b3e79adb1a257ba727465f31e50aa388
Thomas Furio <tommyfurio@gmail.com>
2023-11-23 17:08:07 +0100

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get '/lists/new', to: 'lists#new', as: 'new_list'
+  get '/lists/:id', to: 'lists#show', as: 'list'
+  get '/lists', to: 'lists#index', as: 'lists'
+  post '/lists', to: 'lists#create'
+
+  # bookmarks
+  get '/lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: 'new_list_bookmark'
+  post '/lists/:list_id/bookmarks', to: 'bookmarks#create', as: 'list_bookmarks'
 end
